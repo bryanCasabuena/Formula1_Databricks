fa96436640870d217881b4daf4132804ab6752f2
Bryan Carlo Casabuena <107269070+bryanCasabuena@users.noreply.github.com>
2024-10-09 11:59:36 +0800

    Add files via upload

---
+++ F1_Databricks/includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dl050199/raw"
+processed_folder_path = "/mnt/formula1dl050199/processed"
+presentation_folder_path = "/mnt/formula1dl050199/presentation"
+
+# COMMAND ----------
+
+#raw_folder_path = 'abfss://raw@formula1dl050199.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@formula1dl050199.dfs.core.windows.net'
+#presentation_folder_path = 'abfss://presentation@formula1dl050199.dfs.core.windows.net'
